4cb6206653fa6ba9ebf2558fb90f637f084d3915
Bshën <162020887+mwarcc@users.noreply.github.com>
2024-05-31 11:57:17 -0700

    using uvicorn back lol

--- api.py
+++ api.py
@@ -1,24 +1,22 @@
 from blacksheep import Application, Request
 from mspscammers.routes.registration import register_registration_routes
 from mspscammers.routes.discord_auth import register_discord_routes
+from mspscammers.routes.reporting import register_reporting_routes
 import mspscammers.database as database
 import asyncio
 
 app = Application()
 register_registration_routes(app=app)
 register_discord_routes(app=app)
+register_reporting_routes(app=app)
 
 @app.on_start
 async def on_start():
-    tasks = [
-        database.create_ban_database(),
-        database.create_report_database(),
-        database.create_user_database()
-    ]
-    await asyncio.gather(*tasks)
+    await database.create_ban_database(),
+    await database.create_report_database(),
+    await database.create_user_database()
+
 
 if __name__ == "__main__":
-    import sys
-    from daphne.cli import CommandLineInterface
-
-    CommandLineInterface().run(["api:app"] + sys.argv[1:])
+    import uvicorn
+    uvicorn.run(app=app, host="127.0.0.1", port=8000)
