8927830a9f9b7b2f6032e4de417b530d45cad5df
Bshën <162020887+mwarcc@users.noreply.github.com>
2024-05-31 10:17:30 -0700

    Update registration.py

--- mspscammers/routes/registration.py
+++ mspscammers/routes/registration.py
@@ -3,7 +3,41 @@ from blacksheep.server.responses import json
 from mspscammers import database as db
 from typing import Awaitable, Callable
 
-### This endpoint will never be used, for testing only.
+from typing import Dict, Awaitable, Callable
+
+class DatabaseCache:
+    _cache: Dict[str, Dict[str, bool]]
+
+    def __init__(self):
+        self._cache = {}
+
+    async def check_username_exists_cached(self, username: str) -> bool:
+        if "username" not in self._cache:
+            self._cache["username"] = {}
+
+        if username not in self._cache["username"]:
+            self._cache["username"][username] = await db.check_username_exists(username)
+
+        return self._cache["username"][username]
+
+    async def check_user_exists_cached(self, user_id: str) -> bool:
+        if "user_id" not in self._cache:
+            self._cache["user_id"] = {}
+
+        if user_id not in self._cache["user_id"]:
+            self._cache["user_id"][user_id] = await db.check_user_exists(user_id)
+
+        return self._cache["user_id"][user_id]
+    
+    def update_cache(self, user_id: str, username: str) -> None:
+        if "username" not in self._cache:
+            self._cache["username"] = {}
+        if "user_id" not in self._cache:
+            self._cache["user_id"] = {}
+        self._cache["username"][username] = True
+        self._cache["user_id"][user_id] = True
+
+cache  = DatabaseCache()
 
 async def create_account(request: Request) -> Awaitable[json]:
     data: dict = await request.json()
@@ -14,8 +48,8 @@ async def create_account(request: Request) -> Awaitable[json]:
         error_response = {"detail": missing_fields}
         return json(data=error_response, status=400)
 
-    username_exists = await db.check_username_exists(data['discord_username'])
-    user_exists = await db.check_user_exists(data['discord_user_id'])
+    username_exists = await cache.check_username_exists_cached(data['discord_username'])
+    user_exists = await cache.check_user_exists_cached(data['discord_user_id'])
 
     if username_exists:
         error_response = {
@@ -41,6 +75,9 @@ async def create_account(request: Request) -> Awaitable[json]:
 
     await db.add_user(data['discord_user_id'], request.client_ip, data['discord_username'], data['image_url'])
     user_data = await db.get_user_data_json(data['discord_user_id'])
+
+    if user_data:
+        cache.update_cache(user_id=data['discord_user_id'], username=data['discord_username'])
 
     if user_data:
         success_response = {
